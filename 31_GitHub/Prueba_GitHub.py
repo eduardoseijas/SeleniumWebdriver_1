eae776248292e12cdeabc7e1d44e3dbd642b6a18
Eduardo <eduardoseijas18@gmail.com>
2025-08-17 19:11:29 +0200

    Version 6 guardando y subiendo a github y hacer git push

--- 31_GitHub/Prueba_GitHub.py
+++ 31_GitHub/Prueba_GitHub.py
@@ -1,2 +1,5 @@
 #Esta prueba es despues de crear el repositorio y usar los comando
 #git para salvar esto y aparte subirlo a mi archivo GitHub
+
+#luego de cualquier cambio para que sirva y se vea, hay que hacer un git push
+#despues del git commit -m ""
